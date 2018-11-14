4e9448deda1e14961e1bc1e5c34ee2d8fc9899d4
pengyi <pengyi9627@gmail.com>
2018-11-14 11:49:41 +0800

    init

--- coordinate/start.py
+++ coordinate/start.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import sys
 
-sys.path.append('/root/server/server')
+sys.path.append('/root/server')
 # sys.path.append('/home/pengyi/server/server')
 # sys.path.append('C:\\Users\pengyi\server')
 # sys.path.append('/Users/yi/server')
@@ -9,7 +9,7 @@ sys.path.append('/root/server/server')
 from core import config
 import core.globalvar as gl
 
-config.init("/root/server/server/conf/pyg.conf")
+config.init("/root/server/conf/pyg.conf")
 # config.init("/home/pengyi/server/server/conf/pyg.conf")
 # config.init("C:\\Users\pengyi\server\conf\pyg.conf")
 # config.init("/Users/yi/server/conf/pyg.conf")
