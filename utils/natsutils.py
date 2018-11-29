35b4f8b66ed3e6889fc63e158793841f4bae0b7c
pengyi <pengyi9627@gmail.com>
2018-11-29 12:17:49 +0800

    init

--- utils/natsutils.py
+++ utils/natsutils.py
@@ -14,18 +14,17 @@ class NatsUtils(object):
     @tornado.gen.coroutine
     def main(self):
         yield self.__nc__.connect(self.__servers__)
-        if self.__nc__.is_connected:
-            for i in range(0, len(self.__subject__)):
-                self.__nc__.subscribe(self.__subject__[i], cb=self.__handle__[i])
+        for i in range(0, len(self.__subject__)):
+            yield self.__nc__.subscribe(self.__subject__[i], cb=self.__handle__[i])
 
     def startNats(self):
         self.main()
         tornado.ioloop.IOLoop.instance().start()
 
+    @tornado.gen.coroutine
     def publish(self, subject, data):
-        if self.__nc__.is_connected:
-            self.__nc__.publish(subject, data)
-            self.__nc__.flush()
+        yield self.__nc__.publish(subject, data)
+        yield self.__nc__.flush()
 
     def isConnect(self):
         return self.__nc__.is_connected
