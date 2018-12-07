5f0fa170a73604e2880dc74c97ddb30d08185353
pengyi <pengyi9627@gmail.com>
2018-12-07 12:34:40 +0800

    init

--- gateway_start.py
+++ gateway_start.py
@@ -1,15 +1,10 @@
 # coding=utf-8
 import pkgutil
 
-sys.path.append('/root/server')
-# sys.path.append('/home/pengyi/server/server')
-# sys.path.append('C:\\Users\pengyi\server')
-# sys.path.append('/Users/yi/server')
-
 from core import config
 import core.globalvar as gl
 
-config.init("/root/server/conf/pyg.conf")
+config.init("/root/game/pyg.conf")
 # config.init("/home/pengyi/server/server/conf/pyg.conf")
 # config.init("C:\\Users\pengyi\server\conf\pyg.conf")
 # config.init("/Users/yi/server/conf/pyg.conf")
--- tuitongzi_start.py
+++ tuitongzi_start.py
@@ -4,7 +4,7 @@ import pkg_resources
 from core import config
 import core.globalvar as gl
 
-config.init("/root/server/conf/pyg.conf")
+config.init("/root/game/pyg.conf")
 # config.init("/home/pengyi/server/server/conf/pyg.conf")
 # config.init("C:\\Users\pengyi\server\conf\pyg.conf")
 # config.init("/Users/yi/server/conf/pyg.conf")
