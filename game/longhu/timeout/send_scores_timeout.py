80c0371cbdf907ab3f292e7feb6c85641c1563f9
pengyi <pengyi9627@gmail.com>
2018-12-17 17:18:24 +0800

    init

--- game/longhu/timeout/send_scores_timeout.py
+++ game/longhu/timeout/send_scores_timeout.py
@@ -1,7 +1,9 @@
+# coding=utf-8
 import time
 import traceback
 
 import core.globalvar as gl
+from protocol.base.base_pb2 import EXECUTE_ACTION
 
 
 class SendScores(object):
@@ -21,12 +23,18 @@ class SendScores(object):
 
             redis = gl.get_v("redis")
             if redis.exists("room_" + str(self.roomNo)):
+                gl.get_v("serverlogger").logger.info("下注加锁前")
                 redis.lock("lockroom_" + str(self.roomNo))
                 try:
                     room = redis.getobj("room_" + str(self.roomNo))
                     if 0 != len(room.betScores):
-                        room.sendBetScore(self.messageHandle)
+                        senddata = room.getSendBetScore()
                         room.save(redis)
+                        redis.unlock("lockroom_" + str(self.roomNo))
+                        gl.get_v("serverlogger").logger.info("下注发送前")
+                        self.messageHandle.broadcast_watch_to_gateway(EXECUTE_ACTION, senddata, self)
+                        gl.get_v("serverlogger").logger.info("下注发送完成")
+
                 except:
                     print traceback.print_exc()
-                redis.unlock("lockroom_" + str(self.roomNo))
+                    redis.unlock("lockroom_" + str(self.roomNo))
