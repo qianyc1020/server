3d7f08c8dfab6c8b88941c199611879a6e11e189
pengyi <pengyi9627@gmail.com>
2018-12-18 18:53:44 +0800

    init

--- game/tuitongzi/command/client/shangzhuang_cmd.py
+++ game/tuitongzi/command/client/shangzhuang_cmd.py
@@ -14,7 +14,7 @@ def execute(userId, message, messageHandle):
         roomNo = redis.get(str(userId) + "_room")
         score = BaiRenScore()
         score.ParseFromString(message.data)
-        onlyPlayerBanker = bool(config.get(" tuitongzi", "onlyPlayerBanker"))
+        onlyPlayerBanker = bool(config.get("tuitongzi", "onlyPlayerBanker"))
         redis.lock("lockroom_" + str(roomNo))
         try:
             room = redis.getobj("room_" + str(roomNo))
