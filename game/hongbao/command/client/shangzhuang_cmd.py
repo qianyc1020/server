c51e41de6f13d3dc7705f966c32e7623714e610b
pengyi <pengyi9627@gmail.com>
2018-12-18 19:00:19 +0800

    init

--- game/hongbao/command/client/shangzhuang_cmd.py
+++ game/hongbao/command/client/shangzhuang_cmd.py
@@ -14,7 +14,7 @@ def execute(userId, message, messageHandle):
         roomNo = redis.get(str(userId) + "_room")
         score = BaiRenScore()
         score.ParseFromString(message.data)
-        onlyPlayerBanker = bool(config.get(" hongbao", "onlyPlayerBanker"))
+        onlyPlayerBanker = bool(config.get("hongbao", "onlyPlayerBanker"))
         redis.lock("lockroom_" + str(roomNo))
         try:
             room = redis.getobj("room_" + str(roomNo))
--- mode/base/create_game_details.py
+++ mode/base/create_game_details.py
@@ -2,7 +2,7 @@
 class CreateGameDetails(object):
 
     def __init__(self, user_id, alloc_id, room_no, score, service_charge, time):
-        self.userId = user_id
+        self.user_id = user_id
         self.alloc_id = alloc_id
         self.room_no = room_no
         self.score = score
