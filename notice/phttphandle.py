8b7eb19656db1de2d4a42271c247cdc05e9717f5
pengyi <pengyi9627@gmail.com>
2018-12-20 11:28:00 +0800

    init

--- notice/phttphandle.py
+++ notice/phttphandle.py
@@ -66,7 +66,7 @@ class HttpRequest(object):
             self.request_data = {}
             request_body = body.split('\r\n\r\n', 1)[1]
             parameters = request_body.split('&')  # 每一行是一个字段
-            gl.get_v("serverlogger").logger.info("参数%s" % request_body)
+            gl.get_v("serverlogger").logger.info("参数%s" % str(request_body))
             for i in parameters:
                 if i == '':
                     continue
@@ -78,7 +78,7 @@ class HttpRequest(object):
                 req = self.url.split('?', 1)[1]
                 s_url = self.url.split('?', 1)[0]
                 parameters = req.split('&')
-                gl.get_v("serverlogger").logger.info("参数%s" % req)
+                gl.get_v("serverlogger").logger.info("参数%s" % str(req))
                 for i in parameters:
                     key, val = i.split('=', 1)
                     self.request_data[key] = val
@@ -88,7 +88,7 @@ class HttpRequest(object):
     def do(self, path):
         self.response_line = ErrorCode.NOT_FOUND
         self.response_head['Content-Type'] = 'text/html'
-        gl.get_v("serverlogger").logger.info("路径%s" % path)
+        gl.get_v("serverlogger").logger.info("路径%s" % str(path))
         if path == "/addgold":
             userId = self.request_data["userId"]
             gold = self.request_data["gold"]
@@ -130,7 +130,9 @@ class HttpRequest(object):
                             break
 
     def getResponse(self):
-        return self.response_line + dict2str(self.response_head) + '\r\n' + self.response_body
+        response = self.response_line + dict2str(self.response_head) + '\r\n' + self.response_body
+        gl.get_v("serverlogger").logger.info("返回%s" % response)
+        return response
 
     def sendToGame(self, uuid, opcode, data):
         message = NetMessage()
