72ed72519aacd691569235ce574cd21446d023c4
pengyi <pengyi9627@gmail.com>
2019-06-06 14:48:18 +0700

    init

--- tuitongzi_start.py
+++ tuitongzi_start.py
@@ -3,7 +3,6 @@ import pkg_resources
 
 from core import config
 import core.globalvar as gl
-from protocol.game.zhipai_pb2 import ShuffleData
 
 config.init("./conf/pyg.conf")
 gl.init()
@@ -11,16 +10,4 @@ gl.init()
 from game.tuitongzi.server.server import Server
 
 if __name__ == '__main__':
-    with open('./conf/tuitongzicheat.t') as infile:
-        strs = infile.read()
-        str = strs.split(',')
-
-        shuffleData = ShuffleData()
-        cheatData = shuffleData.cheatData.add()
-        cheatData.level = int(str[0])
-        cheatData = shuffleData.cheatData.add()
-        cheatData.level = int(str[1])
-        cheatData = shuffleData.cheatData.add()
-        cheatData.level = int(str[2])
-        cheatData = shuffleData.cheatData.add()
-        cheatData.level = int(str[3])
+    Server.start()
